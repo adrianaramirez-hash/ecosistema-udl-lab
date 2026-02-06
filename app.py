3538c262611da64e2d636cdfe3dad4fe6a1b9318
adrianaramirez-hash <adriana.ramirez@udlondres.com>
2026-02-06 12:43:21 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Ecosistema UDL - LAB",
+    layout="wide"
+)
+
+st.title("Ecosistema UDL - LAB")
+st.caption("Repositorio laboratorio · No producción")
+
+st.success("Repo creado correctamente. Siguiente paso: configuración.")
